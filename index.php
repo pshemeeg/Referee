6cc15173db06a6fb40b482602fc85ae79ef092c5
pshemeeg <pshemeeg@gmail.com>
2024-06-03 23:26:23 +0200

    Added link in index.php to add-form.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-phpinfo()
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" >
+  <title>Zawody koła pzw 8</title>
+</head>
+<body>
+  <h1>Zawody koła PZW nr 8 w Bieruniu Nowym</h1>
+  <main>
+    <h2>Wybierz zawody z listy lub dodaj nowe.</h2>
+    <ul>
+    </ul>
+    <a href="add-form.php">Dodaj nowe zawody!</a>
+  </main>
+</body>
+</html>
