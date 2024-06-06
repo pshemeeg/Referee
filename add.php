5267a3566fdaf322d858f818350f4c9d5f07718e
pshemeeg <pshemeeg@gmail.com>
2024-06-07 00:58:16 +0200

    Added comments to file add.php

--- add.php
+++ add.php
@@ -1,5 +1,5 @@
 <?php
-
+#Pobranie wartości z formularza do zmiennych !Trzeba dowiedzieć się czy da się to zrobić lepiej!
 $discipline = $_POST['discipline'];
 $category = $_POST['category'];
 $turns = $_POST['turns'];
@@ -16,19 +16,19 @@ $secretary = $_POST['secretary'];
 $sectorRef = $_POST['sectorRef'];
 $controlRef = $_POST['controlRef'];
 $terrain = $_POST['terrain'];
-
+#Otwarcie połączenia z bazą danych
 $mysqli = new mysqli('localhost','root','','referee');
-
+#Sprawdzenie czy połączenie się udało
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
-
+#Kwerenda SQL do wykonania na bazie danych dodanie nowych zawodów do tabeli "zawody" !Trzeba to przerobić na sprintf żeby jakoś wyglądało!
 $sql = "INSERT INTO `zawody`(`discipline`, `category`, `turns`, `date`, `startTime`, `endTime`, `water`, `city`, `name`, `organiser`, `region`, `referee`, `secretary`, `sectorRef`, `controlRef`, `terrain`) VALUES ('$discipline','$category','$turns','$date','$startTime','$endTime','$water','$city','$name','$organiser','$region','$referee','$secretary','$sectorRef','$controlRef','$terrain')";
-
-
+#Wykonanie kwerendy na bazie danych
 $mysqli -> query($sql); 
-
+#Zamknięcie połączenia z bazą
 $mysqli -> close();
+#Przekierowanie na stronę główną
 header('Location:index.php')
 ?>
